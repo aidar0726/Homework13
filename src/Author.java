23df518c2990ef18232c9adf374f25605a4c2772
shakirov_a <Shakirov0726@gmail.com>
2022-11-02 19:23:24 +0600

    ДЗ 13 (методы объектов)

---
+++ src/Author.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class Author {
+    private String authorName;
+    private String authorSurname;
+
+    public Author(String authorName, String authorSurname) {
+        this.authorName = authorName;
+        this.authorSurname = authorSurname;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getAuthorSurname() {
+        return authorSurname;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Author c2 = (Author) other;
+        return authorName.equals(c2.authorName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(authorName);
+    }
+
+    @Override
+    public String toString() {
+        return "Обект Author: " + "Имя автора: " + authorName + " , Фамилия автора: " + authorSurname;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,47 @@
+import java.util.Objects;
+
+public class Book {
+    private Author authorName;
+
+    private String bookName;
+    private int publishingYear;
+
+    public Book(Author authorName, String bookName, int publishingYear) {
+        this.authorName = authorName;
+        this.bookName = bookName;
+        this.publishingYear = publishingYear;
+    }
+
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+
+        this.publishingYear = publishingYear;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book c2 = (Book) other;
+        return bookName.equals(c2.bookName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName);
+    }
+
+    @Override
+    public String toString() {
+        return authorName.toString();
+    }
+}
